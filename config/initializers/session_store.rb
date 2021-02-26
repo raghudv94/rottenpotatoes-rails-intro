4992f701178f01d0dc3da9c2edeee6e897e6d32e
Raghu D Veerappa <raghudv94@gmail.com>
2021-02-26 04:22:08 +0000

    minor fix hw2 part 3- clear session on browser and tab exit

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-#Rails.application.config.session_store :cookie_store, key: '_rottenpotatoes_session', expire_after: nil
-Rails.application.config.session_store :disabled
+Rails.application.config.session_store :cookie_store, key: '_rottenpotatoes_session', expire_after: nil
+#Rails.application.config.session_store :disabled
